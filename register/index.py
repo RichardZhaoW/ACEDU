007a46a2028135a091e79ffa37b71ee5ac568d91
RichardZhaoW <richard.w.zhao@outlook.com>
2018-05-13 20:53:03 +0800

    login

--- register/index.py
+++ register/index.py
@@ -1,11 +1,69 @@
 # -*- coding: UTF-8 -*-
 #coding=utf-8
-from flask import Flask, render_template, flash, redirect, request
+from flask import Flask, render_template, flash, redirect, request, url_for, abort
+from flask.ext.login import LoginManager, UserMixin, login_user, logout_user
+from flask.ext.login import current_user, login_required, fresh_login_required
 import pymysql
+
 app = Flask(__name__,static_folder='', static_url_path='', template_folder='')
+app.secret_key = '1234567'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Unauthorized User'
+login_manager.login_message_category = "info"
+
 SQLHOST = "rm-2zex7u1bczx7a738v.mysql.rds.aliyuncs.com"
+users = [{'username':'admin', 'password':'admin'}, {'username':'test', 'password':'test'}]
+class User(UserMixin):
+    pass
+
+def query_user(username):
+    for user in users:
+        if user['username'] == username:
+            return user
+
+@login_manager.user_loader
+def load_user(username):
+    if query_user(username):
+        curr_user = User()
+        curr_user.id = username
+        return curr_user
+    return None
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        user = query_user(username)
+        # 验证表单中提交的用户名和密码
+        if user is not None and request.form['password'] == user['password']:
+            curr_user = User()
+            curr_user.id = username
+
+            # 通过Flask-Login的login_user方法登录用户
+            login_user(curr_user, remember=True)
+
+            # 如果请求中有next参数，则重定向到其指定的地址，
+            # 没有next参数，则重定向到"index"视图
+            next = request.args.get('next')
+            return redirect(next or url_for('index'))
+
+        flash('Wrong username or password!')
+    # GET 请求
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'Logged out successfully!'
 
 @app.route("/register", methods=['GET','POST'])
+@login_required
 def register():
     if request.method == 'POST':
         form = request.form
@@ -13,9 +71,9 @@ def register():
         print sql
         bOK = executeSQL(sql)
         if bOK:
-            return output(form)
+            return u'报名成功'#outputInfo(form)
         else:
-            return u'注册失败\n请联系ioe-te@pku.edu.cn'
+            return u'报名失败,\n请联系ioe-te@pku.edu.cn'
     return render_template('index.html')
 
 def outputInfo(form):
@@ -67,4 +125,4 @@ def executeSQL(sql):
     return bOK, rows
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
