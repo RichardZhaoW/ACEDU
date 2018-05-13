69696a55ee0edb6518d9cca3bf8f2a0fcbfca0fe
RichardZhaoW <richard.w.zhao@outlook.com>
2018-05-13 12:32:29 +0800

    encoding

--- register/index.py
+++ register/index.py
@@ -8,17 +8,17 @@ def register():
     if request.method == 'POST':
         form = request.form
         print getSQL(form)
-        return '注册成功'
+        return u'注册成功'
     return render_template('index.html')
 
 def getSQL(form):
-    sql = "INSERT INTO acedu.register" \
+    sql = u"INSERT INTO acedu.register" \
           "(idcard, mobile, fullname, email, school, gender, level, subject, " \
           "occurence, activityType, isLunch, isMoslem, isShuttle, dateArrival, dateDeparture)" \
           "values({idcard}, {mobile}, '{fullname}', '{email}', '{school}', '{gender}', '{level}', '{subject}', " \
           "{occurence}, {activityType}, {isLunch}, {isMoslem}, {isShuttle}, '{dateArrival}', '{dateDeparture}');".format(
         idcard=form['idcard'], mobile=form['mobile'], fullname=form['fullname'], email=form['email'],
-        school=form['school'], gender=form['gender'], subject=form['subject'],
+        school=form['school'], gender=form['gender'], level=form['level'], subject=form['subject'],
         occurence=form['occurence'], activityType=form['activityType'], isLunch=form['isLunch'], isMoslem=form['isMoslem'],
          isShuttle=form['isShuttle'], dateArrival=form['dateArrival'], dateDeparture=form['dateDeparture']
     )
